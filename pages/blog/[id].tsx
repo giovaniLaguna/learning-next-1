d7a034d3bc0c3b969255e36759718cba28a45f0e
Giovani Henrique Laguna <giovani.laguna@performait.com>
2023-03-22 20:08:02 -0300

    Aprendendo sobre revalidate, notFound, fallback

--- pages/blog/[id].tsx
+++ pages/blog/[id].tsx
@@ -2,11 +2,19 @@ import { Post } from "@/data/blog/post";
 import { getPostById, getPostsId } from "@/services/blog/posts.mock";
 import { Router, useRouter } from "next/router";
 import { useEffect } from "react";
+import styles from '@/styles/Home.module.css';
 
 type PostProps = { post: Post };
 
 export default function BlogPost({ post }: PostProps) {
- return (
+  const router = useRouter();
+
+  // Quando uma parte da aplicação esta sendo 
+  if(!post) {
+    return <span style={{ 'backgroundColor': 'red' }}>Loading</span>
+  }
+
+  return (
     <>
       <a onClick={() => router.back()}>Go back</a>
       <article>
@@ -22,11 +30,11 @@ export async function getStaticPaths() {
 
   const paths = postId.map((post) => ({
     params: { id: post },
-  }));
+  })).filter((_, i) => i === 0);
 
   return {
     paths,
-    fallback: false,
+    fallback: true,
   };
 }
 
@@ -34,9 +42,18 @@ type PostStaticProps = { params: { id: string } };
 export async function getStaticProps({ params }: PostStaticProps) {
   const post = await getPostById(params.id);
 
+  console.log("IIID", post);
+
+  if (!post) {
+    return {
+      notFound: true,
+    };
+  }
+
   return {
     props: {
       post,
     },
+    revalidate: 50,
   };
 }
--- services/blog/posts.mock.ts
+++ services/blog/posts.mock.ts
@@ -1,21 +1,43 @@
 import { Post, PostHeader } from '@/data/blog/post';
+import fs from 'fs/promises';
 
-const posts: Post[] = [
-  { title: "Post1", id: "1", content: '' },
-  { title: "Post2", id: "2", content: '' },
-  { title: "Post3", id: "3", content: '' },
-];
+async function getPosts(): Promise<Post[]> {
+  const filePath = new URL("file://C:/ArquivosZ/projetos/learning-nextjs/learning-1/services/blog/posts.json");
+  console.log(filePath.toString());
+  const contents = await fs.readFile(filePath, { encoding: 'utf8' });
+  console.log(contents);
+  const posts = JSON.parse(contents);
+
+  if(posts[0]) {
+    posts[0].content = (new Date()).toISOString();
+  }
+
+  return posts;
+}
 
 export async function getPostsHeader(): Promise<PostHeader[]> {
+  const posts = await getPosts();
+
   const postHeader = posts.map(post => ({ id: post.id, title: post.title }));
 
   return postHeader;
 }
 
 export async function getPostById(id: string): Promise<Post | undefined> {
+  const posts = await getPosts();
+
+  // if (id === 'apareceFuturamente' && !posts.find( p => p.id === 'apareceFuturamente')) {
+  //   setTimeout(() => {
+  //     posts.push({ title: "apareceFuturamente", content: '', id: 'apareceFuturamente'});
+  //     console.log('Adicionado o aparece futuramente');
+  //   }, 3000)
+  //   console.log('UIA')
+  // }
   return posts.find(post => post.id === id)
 }
 
 export async function getPostsId(): Promise<string[]> {
+  const posts = await getPosts();
+
   return posts.map( post => post.id );
 }
